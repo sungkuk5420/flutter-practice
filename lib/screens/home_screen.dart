d7c06c901859f43fb2fbbb3a4fa7ca3159528dbb
sungkuk5420 <sungkuk5420@gmail.com>
2023-03-15 12:26:09 +0900

    스테이트 없이 진행할 수 있는 위젯 퓨쳐 빌더 사용해보기.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,47 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:toonflix/models/webtoon.dart';
 import 'package:toonflix/services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoons = [];
-
-  bool isLoading = true;
-
-  void waitForWebToons() async {
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-
-    waitForWebToons();
-  }
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
-    print(webtoons);
-    print(isLoading);
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 2,
-        foregroundColor: Colors.green,
         backgroundColor: Colors.white,
-        title: const Text(
-          "오늘의 웹툰",
-          style: TextStyle(fontSize: 24),
+        appBar: AppBar(
+          elevation: 2,
+          foregroundColor: Colors.green,
+          backgroundColor: Colors.white,
+          title: const Text(
+            "오늘의 웹툰",
+            style: TextStyle(fontSize: 24),
+          ),
         ),
-      ),
-    );
+        body: FutureBuilder(
+          future: webtoons,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return const Text("There is data!");
+            }
+            return const Text("Loading...");
+          },
+        ));
   }
 }
