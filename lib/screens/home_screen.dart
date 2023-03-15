f73a60c2a24f91215623941c50e1cf11d83dc9a8
sungkuk5420 <sungkuk5420@gmail.com>
2023-03-15 11:45:44 +0900

    웹툰 api 받아오기 기본 방법.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
+import 'package:toonflix/models/webtoon.dart';
+import 'package:toonflix/services/api_service.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,132 +10,38 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  static const twentyFiveMinutes = 1500;
-  int totalSeconds = twentyFiveMinutes;
-  int totalPomodros = 0;
-  bool isRunning = false;
-  late Timer timer;
+  List<WebtoonModel> webtoons = [];
 
-  String format(int seconds) {
-    var duration = Duration(seconds: seconds);
-    return duration.toString().split(".").first.substring(2, 7);
+  bool isLoading = true;
+
+  void waitForWebToons() async {
+    webtoons = await ApiService.getTodaysToons();
+    isLoading = false;
+    setState(() {});
   }
 
-  void onTick(Timer timer) {
-    if (totalSeconds == 0) {
-      setState(() {
-        isRunning = false;
-        totalSeconds = twentyFiveMinutes;
-        totalPomodros = totalPomodros + 1;
-      });
-      timer.cancel();
-    } else {
-      setState(() {
-        totalSeconds = totalSeconds - 1;
-      });
-    }
-  }
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
 
-  void onStartPressed() {
-    timer = Timer.periodic(const Duration(seconds: 1), onTick);
-    setState(() {
-      isRunning = true;
-    });
-  }
-
-  void onPausePressed() {
-    timer.cancel();
-    setState(() {
-      isRunning = false;
-    });
-  }
-
-  void onResetPressed() {
-    timer.cancel();
-    setState(() {
-      isRunning = false;
-      totalSeconds = twentyFiveMinutes;
-    });
+    waitForWebToons();
   }
 
   @override
   Widget build(BuildContext context) {
+    print(webtoons);
+    print(isLoading);
     return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
-      body: Column(
-        children: [
-          Flexible(
-            flex: 1,
-            child: Container(
-              alignment: Alignment.bottomCenter,
-              child: Text(
-                format(totalSeconds),
-                style: TextStyle(
-                    color: Theme.of(context).cardColor,
-                    fontSize: 89,
-                    fontWeight: FontWeight.w600),
-              ),
-            ),
-          ),
-          Flexible(
-            flex: 1,
-            child: Center(
-              child: Column(
-                children: [
-                  IconButton(
-                      iconSize: 120,
-                      color: Theme.of(context).cardColor,
-                      onPressed: isRunning ? onPausePressed : onStartPressed,
-                      icon: isRunning
-                          ? const Icon(Icons.pause_circle_outline)
-                          : const Icon(Icons.play_circle_outline)),
-                  IconButton(
-                    color: Theme.of(context).cardColor,
-                    onPressed: onResetPressed,
-                    icon: const Icon(Icons.restore),
-                  )
-                ],
-              ),
-            ),
-          ),
-          Flexible(
-            flex: 1,
-            child: Row(
-              children: [
-                Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).cardColor,
-                        borderRadius: const BorderRadius.only(
-                            topLeft: Radius.circular(50),
-                            topRight: Radius.circular(50))),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          'Pomodors',
-                          style: TextStyle(
-                              fontSize: 20,
-                              fontWeight: FontWeight.w600,
-                              color:
-                                  Theme.of(context).textTheme.headline1?.color),
-                        ),
-                        Text(
-                          '$totalPomodros',
-                          style: TextStyle(
-                              fontSize: 58,
-                              fontWeight: FontWeight.w600,
-                              color:
-                                  Theme.of(context).textTheme.headline1?.color),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 2,
+        foregroundColor: Colors.green,
+        backgroundColor: Colors.white,
+        title: const Text(
+          "오늘의 웹툰",
+          style: TextStyle(fontSize: 24),
+        ),
       ),
     );
   }
