78c99e85c8ccbc36ecf580e2a07d690bc2b3cc41
sungkuk5420 <sungkuk5420@gmail.com>
2023-04-24 08:49:41 +0900

    웹툰 상세 페이지 완성

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/models/webtoon.dart';
 import 'package:toonflix/services/api_service.dart';
+import 'package:toonflix/widgets/webtoon_widgets.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
@@ -10,34 +11,35 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 2,
+        foregroundColor: Colors.green,
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          elevation: 2,
-          foregroundColor: Colors.green,
-          backgroundColor: Colors.white,
-          title: const Text(
-            "오늘의 웹툰",
-            style: TextStyle(fontSize: 24),
-          ),
+        title: const Text(
+          "오늘의 웹툰",
+          style: TextStyle(fontSize: 24),
         ),
-        body: FutureBuilder(
-          future: webtoons,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Column(
-                children: [
-                  const SizedBox(height: 50),
-                  Expanded(
-                    child: makeList(snapshot),
-                  )
-                ],
-              );
-            }
-            return const Center(
-              child: CircularProgressIndicator(),
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Column(
+              children: [
+                const SizedBox(height: 50),
+                Expanded(
+                  child: makeList(snapshot),
+                )
+              ],
             );
-          },
-        ));
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
   }
 
   ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
@@ -47,33 +49,8 @@ class HomeScreen extends StatelessWidget {
       itemCount: snapshot.data!.length,
       itemBuilder: (context, index) {
         var webtoon = snapshot.data![index];
-        return Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                        blurRadius: 15,
-                        offset: const Offset(10, 10),
-                        color: Colors.black.withOpacity(0.5))
-                  ]),
-              width: 250,
-              clipBehavior: Clip.hardEdge,
-              child: Image.network(
-                webtoon.thumb,
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Text(webtoon.title,
-                style: const TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.w600,
-                )),
-          ],
-        );
+        return Webtoon(
+            title: webtoon.title, thumb: webtoon.thumb, id: webtoon.id);
       },
       separatorBuilder: (context, index) => const SizedBox(width: 40),
     );
---
+++ lib/widgets/detail_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  final String title, thumb, id;
+  const DetailScreen({
+    super.key,
+    required this.title,
+    required this.thumb,
+    required this.id,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 2,
+        foregroundColor: Colors.green,
+        backgroundColor: Colors.white,
+        title: Text(
+          title,
+          style: const TextStyle(fontSize: 24),
+        ),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          blurRadius: 15,
+                          offset: const Offset(10, 10),
+                          color: Colors.black.withOpacity(0.5))
+                    ]),
+                width: 250,
+                clipBehavior: Clip.hardEdge,
+                child: Image.network(
+                  thumb,
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
